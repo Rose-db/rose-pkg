f8354937b7de279c8a38d6c447308bc7174776a1
Mario Skrlec <mario@prototyp.digital>
2020-12-29 12:52:04 +0100

    Splitted query queue and balancer into two separate files

--- balancer.go
+++ balancer.go
@@ -1,10 +1,6 @@
 package rose
 
 import (
-	"fmt"
-	"github.com/valyala/fastjson"
-	"os"
-	"strings"
 	"sync"
 )
 
@@ -13,20 +9,11 @@ type QueryResult struct {
 	Data []uint8
 }
 
-type queryQueue struct {
-	Comm []chan *queueItem
-}
-
 type balancer struct {
 	Count uint16
 	Next uint16
 	queryQueue *queryQueue
 	sync.RWMutex
-}
-
-type queueResponse struct {
-	ID int
-	Body []uint8
 }
 
 type balancerRequest struct {
@@ -38,128 +25,6 @@ type balancerRequest struct {
 		DataType dataType
 	}
 	Response chan *queueResponse
-}
-
-type queueItem struct {
-	BlockId uint16
-	CollName string
-	Field string
-	Value interface{}
-	dataType dataType
-	Response chan interface{}
-}
-
-func newQueryQueue(workerNum uint16) *queryQueue {
-	qq := &queryQueue{
-		Comm: make([]chan *queueItem, 0),
-	}
-
-	qq.spawn(workerNum)
-
-	return qq
-}
-
-func (qq *queryQueue) spawn(workerNum uint16) {
-	var i uint16
-	for i = 0; i < workerNum; i++ {
-		c := make(chan *queueItem)
-		qq.Comm = append(qq.Comm, c)
-
-		go qq.runWorker(c)
-	}
-}
-
-func (qq *queryQueue) Close() {
-	for _, c := range qq.Comm {
-		if c != nil {
-			close(c)
-		}
-	}
-}
-
-func (qq *queryQueue) runWorker(c chan *queueItem) {
-	for item := range c {
-		blockPath := roseBlockFile(item.BlockId, fmt.Sprintf("%s/%s", roseDbDir(), item.CollName))
-
-		file, err := createFile(blockPath, os.O_RDONLY)
-
-		if err != nil && strings.Contains(err.Error(), "too many open") {
-			file, err = secureBlockingCreateFile(blockPath, os.O_RDONLY)
-
-			if err != nil {
-				item.Response<- &queryError{
-					Code:    QueryErrorCode,
-					Message: fmt.Sprintf("Query resulted in an error: %s", err.Error()),
-				}
-
-				item.Response<- true
-			}
-		}
-
-		if err != nil {
-			item.Response<- &queryError{
-				Code:    QueryErrorCode,
-				Message: fmt.Sprintf("Query resulted in an error: %s", err.Error()),
-			}
-
-			item.Response<- true
-
-			continue
-		}
-
-		reader := NewLineReader(file)
-		var p fastjson.Parser
-
-		for {
-			_, d, err := reader.Read()
-
-			if err != nil && err.GetCode() == EOFErrorCode {
-				break
-			}
-
-			if d == nil {
-				item.Response<- &queryError{
-					Code:    QueryErrorCode,
-					Message: "Unable to read a row during query search",
-				}
-
-				break
-			}
-
-			v, jErr := p.Parse(string(d.val))
-
-			if jErr != nil {
-				item.Response<- &queryError{
-					Code:    QueryErrorCode,
-					Message: fmt.Sprintf("Query resulted in an error: %s", jErr.Error()),
-				}
-
-				break
-			}
-
-			if v.Exists(item.Field) {
-				res := v.GetStringBytes(item.Field)
-
-				if string(res) == item.Value.(string) {
-					item.Response<- &queueResponse{
-						ID:   d.id,
-						Body: d.val,
-					}
-				}
-			}
-		}
-
-		if err := closeFile(file); err != nil {
-			item.Response<- &queryError{
-				Code:    QueryErrorCode,
-				Message: fmt.Sprintf("Query resulted in an error: %s", err.Error()),
-			}
-		}
-
-		reader.Close()
-
-		item.Response<- true
-	}
 }
 
 func newBalancer(currentBlockCount uint16) *balancer {
---
+++ query_queue.go
@@ -0,0 +1,139 @@
+package rose
+
+import (
+	"fmt"
+	"github.com/valyala/fastjson"
+	"os"
+	"strings"
+)
+
+type queryQueue struct {
+	Comm []chan *queueItem
+}
+
+type queueItem struct {
+	BlockId uint16
+	CollName string
+	Field string
+	Value interface{}
+	dataType dataType
+	Response chan interface{}
+}
+
+type queueResponse struct {
+	ID int
+	Body []uint8
+}
+
+func newQueryQueue(workerNum uint16) *queryQueue {
+	qq := &queryQueue{
+		Comm: make([]chan *queueItem, 0),
+	}
+
+	qq.spawn(workerNum)
+
+	return qq
+}
+
+func (qq *queryQueue) Close() {
+	for _, c := range qq.Comm {
+		if c != nil {
+			close(c)
+		}
+	}
+}
+
+func (qq *queryQueue) spawn(workerNum uint16) {
+	var i uint16
+	for i = 0; i < workerNum; i++ {
+		c := make(chan *queueItem)
+		qq.Comm = append(qq.Comm, c)
+
+		go qq.runWorker(c)
+	}
+}
+
+func (qq *queryQueue) runWorker(c chan *queueItem) {
+	for item := range c {
+		blockPath := roseBlockFile(item.BlockId, fmt.Sprintf("%s/%s", roseDbDir(), item.CollName))
+
+		file, err := createFile(blockPath, os.O_RDONLY)
+
+		if err != nil && strings.Contains(err.Error(), "too many open") {
+			file, err = secureBlockingCreateFile(blockPath, os.O_RDONLY)
+
+			if err != nil {
+				item.Response<- &queryError{
+					Code:    QueryErrorCode,
+					Message: fmt.Sprintf("Query resulted in an error: %s", err.Error()),
+				}
+
+				item.Response<- true
+			}
+		}
+
+		if err != nil {
+			item.Response<- &queryError{
+				Code:    QueryErrorCode,
+				Message: fmt.Sprintf("Query resulted in an error: %s", err.Error()),
+			}
+
+			item.Response<- true
+
+			continue
+		}
+
+		reader := NewLineReader(file)
+		var p fastjson.Parser
+
+		for {
+			_, d, err := reader.Read()
+
+			if err != nil && err.GetCode() == EOFErrorCode {
+				break
+			}
+
+			if d == nil {
+				item.Response<- &queryError{
+					Code:    QueryErrorCode,
+					Message: "Unable to read a row during query search",
+				}
+
+				break
+			}
+
+			v, jErr := p.Parse(string(d.val))
+
+			if jErr != nil {
+				item.Response<- &queryError{
+					Code:    QueryErrorCode,
+					Message: fmt.Sprintf("Query resulted in an error: %s", jErr.Error()),
+				}
+
+				break
+			}
+
+			if v.Exists(item.Field) {
+				res := v.GetStringBytes(item.Field)
+
+				if string(res) == item.Value.(string) {
+					item.Response<- &queueResponse{
+						ID:   d.id,
+						Body: d.val,
+					}
+				}
+			}
+		}
+
+		if err := closeFile(file); err != nil {
+			item.Response<- &queryError{
+				Code:    QueryErrorCode,
+				Message: fmt.Sprintf("Query resulted in an error: %s", err.Error()),
+			}
+		}
+
+		reader.Close()
+
+		item.Response<- true
+	}
+}
