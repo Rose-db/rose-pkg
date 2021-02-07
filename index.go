e139b4736bb00096c96c8479d8e2dd45b7cee8bf
Mario Skrlec <mario@prototyp.digital>
2021-02-07 19:12:14 +0100

    Create code that create initial index file if not exists

---
+++ index.go
@@ -0,0 +1,20 @@
+package rose
+
+type IndexFsHandler struct {
+
+}
+
+type Index struct {
+
+}
+
+func NewIndex(collName string, fieldName string) {
+/*	idxRep := fmt.Sprintf("%s%s%s", collName, delim, fieldName)
+	idxLoc := roseIndexLocation()
+
+	stat, err := os.Stat(idxLoc)
+
+	if err != nil {
+		return newError()
+	}*/
+}
